9a38f0d76b70a0b7f75f43a639ad1703bc3db045
Dimara-Delmar <dimaraborges@knights.ucf.edu>
2023-01-30 17:42:07 -0500

    Adding files + Updating Challenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,10 +2,21 @@
 library(tidyverse)
 
 # Import/read the MechaCar_mpg.csv as a dataframe.
-MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 
 # Perform linear regression using the lm() function.
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_table)
 
-# Generate summary statistics
+# Generate summary statistics.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_table))
+
+# Import/read the Suspension_Coil.csv as a dataframe.
+Suspension_table <- read.csv(file='Suspension_Coil.csv',check.names = F, stringsAsFactors = F)
+
+# Create a total_summary dataframe.
+total_summary <- Suspension_table %>% 
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+# Create a lot_summary table.
+lot_summary <- Suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+  
