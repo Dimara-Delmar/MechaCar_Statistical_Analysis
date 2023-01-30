f3674fd873bc8ab872d0186ca87ef73391027f49
Dimara-Delmar <dimaraborges@knights.ucf.edu>
2023-01-30 18:29:56 -0500

    Updating Challenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+# Deliverable 1:
 # Load the dplyr package.
 library(tidyverse)
 
@@ -10,13 +11,28 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Generate summary statistics.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_table))
 
+
+# Deliverable 2:
 # Import/read the Suspension_Coil.csv as a dataframe.
 Suspension_table <- read.csv(file='Suspension_Coil.csv',check.names = F, stringsAsFactors = F)
 
 # Create a total_summary dataframe.
-total_summary <- Suspension_table %>% 
-  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+total_summary <- Suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
 
 # Create a lot_summary table.
 lot_summary <- Suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
-  
+
+
+# Deliverable 3:
+# Use the t.test() function on PSI.
+t.test(Suspension_table$PSI, mu=1500)
+
+# Use the subset() arguments to divide the three lots. 
+psi_lot1 <- Suspension_table %>% subset(Manufacturing_Lot=='Lot1')
+psi_lot2 <- Suspension_table %>% subset(Manufacturing_Lot=='Lot2')
+psi_lot3 <- Suspension_table %>% subset(Manufacturing_Lot=='Lot3')
+
+# Run t.tests on the subsets.
+t.test(psi_lot1$PSI, mu=1500)
+t.test(psi_lot2$PSI, mu=1500)
+t.test(psi_lot3$PSI, mu=1500)
